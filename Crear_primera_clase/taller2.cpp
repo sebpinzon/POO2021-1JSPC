feb280d4c23fc95bf8cc8d03e0fd8d2649e358e8
Juan Sebastian Pinzon <sebpinzon3212@gmail.com>
2021-02-28 18:22:43 -0500

    finished task C && Added Triangulo class

--- Crear_primera_clase/taller2.cpp
+++ Crear_primera_clase/taller2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 
 
 class Circulo
@@ -139,21 +140,198 @@ Circulo crearCirculo()
 	return CI;
 }
 
+class Triangulo
+{
+	private:
+		float LADO_A, LADO_B, LADO_C;
+		float ANGU_A_B, ANGU_B_C, ANGU_C_A;
+	public:
+		Triangulo();
+		Triangulo(float, float, float,float,float,float);
+		float calcularArea();
+		float calcularPerimetro();
+		float getLado();
+		void setLado(float);
+		float getAngulo();
+		void setAngulo(float);
+};
+
+Triangulo::Triangulo()
+{
+	this->LADO_A = 0;
+	this->LADO_B = 0;
+	this->LADO_C = 0;
+	this->ANGU_A_B = 0;
+	this->ANGU_B_C = 0;
+	this->ANGU_C_A = 0;
+}
+
+Triangulo::Triangulo(float LADO_A, float LADO_B, float LADO_C, float ANGU_A_B, float ANGU_B_C, float ANGU_C_A)
+{
+	this->LADO_A = LADO_A;
+	this->LADO_B = LADO_B;
+	this->LADO_C = LADO_C;
+	this->ANGU_A_B = ANGU_A_B;
+	this->ANGU_B_C = ANGU_B_C;
+	this->ANGU_C_A = ANGU_C_A;
+}
+
+void Triangulo::setLado(float value)
+{
+	int OPC;
+	std::cout << "1.Lado A" << "2.Lado B" << "3.Lado C" << std::endl;
+	std::cin >> OPC;
+	if (OPC == 1)
+	{
+		this->LADO_A = value;
+	}
+	else if (OPC == 2)
+	{
+		this->LADO_B = value;	
+	}
+	else if (OPC == 3)
+	{
+		this->LADO_C = value;	
+	}
+	else
+	{
+		std::cout << "Lado no valido"<< std::endl;
+	}
+	
+}
+
+void Triangulo::setAngulo(float value)
+{
+	int OPC;
+	std::cout << "1.Angulo A-B" << "2.Angulo B-C" << "3.Angulo C-A" << std::endl;
+	std::cin >> OPC;
+	if (OPC == 1)
+	{
+		this->ANGU_A_B = value;
+	}
+	else if (OPC == 2)
+	{
+		this->ANGU_B_C = value;	
+	}
+	else if (OPC == 3)
+	{
+		this->ANGU_C_A = value;	
+	}
+	else
+	{
+		std::cout << "Angulo no valido"<< std::endl;
+	}
+}
+
+float Triangulo::getLado()
+{
+	int OPC;
+	float S;
+	std::cout << "1.Lado A" << "2.Lado B" << "3.Lado C" << std::endl;
+	std::cin >> OPC;
+	if (OPC == 1)
+	{
+		S=this->LADO_A;
+	}
+	else if (OPC == 2)
+	{
+		S=this->LADO_B;	
+	}
+	else if (OPC == 3)
+	{
+		S=this->LADO_C;	
+	}
+	else
+	{
+		std::cout << "Lado no valido"<< std::endl;
+	}
+
+	return S;
+}
+
+float Triangulo::getAngulo()
+{
+	int OPC;
+	float S;
+	std::cout << "1.Angulo A-B" << "2.Angulo B-C" << "3.Angulo C-A" << std::endl;
+	std::cin >> OPC;
+	if (OPC == 1)
+	{
+		S=this->ANGU_A_B;
+	}
+	else if (OPC == 2)
+	{
+		S=this->ANGU_B_C;	
+	}
+	else if (OPC == 3)
+	{
+		S=this->ANGU_C_A;	
+	}
+	else
+	{
+		std::cout << "Angulo no valido"<< std::endl;
+	}
+
+	return S;
+}
+
+float Triangulo::calcularPerimetro()
+{
+	float perimetro;
+	perimetro = LADO_A+LADO_B+LADO_C;
+	return perimetro;
+}
+
+float Triangulo::calcularArea()
+{
+	float perimetro, s, area;
+	perimetro = calcularPerimetro();
+	s = perimetro/2;
+	area = sqrt(s*(s-LADO_A)*(s-LADO_B)*(s-LADO_C));
+	return area;
+}
+
+Triangulo crearTriangulo()
+{
+	float L_a, L_b, L_c;
+	float A_a_b, A_b_c, A_c_a;
+	std::cout << "A " << "/" << "\\" << "B" << std::endl;
+	std::cout << "  __ " << std::endl;
+	std::cout << "  C " << std::endl;
+	std::cout << "Lado A del Triangulo:" << std::endl;
+	std::cin >> L_a;
+	std::cout << "Lado B del Triangulo:" << std::endl;
+	std::cin >> L_b;
+	std::cout << "Lado C del Triangulo:" << std::endl;
+	std::cin >> L_c;
+	std::cout << "Angulo entre A y B:" << std::endl;
+	std::cin >> A_a_b;
+	std::cout << "Angulo entre B y C:" << std::endl;
+	std::cin >> A_b_c;
+	std::cout << "Angulo entre C y A:" << std::endl;
+	std::cin >> A_c_a;
+	Triangulo TRI(L_a, L_b, L_c, A_a_b, A_b_c, A_c_a);
+	return TRI;
+}
+
 
 
 int main()
 {
-	int SIZE=10, OPC=0, count_R=0, count_C=0;
+	int SIZE=10, OPC=0, count_R=0, count_C=0, count_T=0;
 	bool STOP = true;
-	Rectangulo REC;
+	
 	Circulo CIR;
+	Rectangulo REC;
+	Triangulo TRI;
 	Circulo circulos[SIZE];
 	Rectangulo rectangulos[SIZE];
+	Triangulo triangulos[SIZE];
 
 
 	while(STOP)
 	{
-		std::cout << "1.Crear rectangulo" << "\n" << "2.Crear circulo" << "\n" << "3.Salir\n" << std::endl;
+		std::cout << "1.Crear rectangulo" << "\n" << "2.Crear circulo" << "\n" << "3.Crear triangulo" << "\n" << "4.Salir\n" << std::endl;
 		std::cin >> OPC;
 
 		switch(OPC)
@@ -200,6 +378,26 @@ int main()
 			}
 			break;
 			case 3:
+			{
+				try{
+					if (count_T >= 9)
+					{
+						throw (count_T);
+						break;
+					}
+					TRI = crearTriangulo();
+					triangulos[count_T] = TRI;
+					count_T++;				
+					
+				}
+				catch(int count_T)
+				{
+					std::cout << "No hay más espacio para guardar Triangulos\n" << std::endl;
+				}
+				
+			}
+			break;
+			case 4:
 			{
 				STOP = false;
 				
